416537d0da1b901fa017e699f7097d95c6a60527
Marek Vcelak <vcelak.marek@gmail.com>
2022-11-02 01:20:42 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,15 @@
 let rangeValue = document.getElementById("password-length-range");
-const characterLength = document.getElementsByClassName("number-of-characters")[0];
+let characterLength = document.getElementsByClassName("number-of-characters")[0];
 
-
-rangeValue.addEventListener("change", () => {
+//rangeValue.addEventListener("change", () => {
     rangeValue.addEventListener("mousemove", () => {
         characterLength.textContent = rangeValue.value;
     })
-})
+//})
 
 let checkboxlist = document.getElementById("checkbox-list");
-
-
-
-
-
 let strength = document.getElementsByTagName("progress")[0];
-
-
-let progressBar = document.getElementsByTagName("progress")[0]; //repair
-
+let progressBar = document.getElementsByTagName("progress")[0];
 let strengthLevel = document.getElementsByClassName("strength-level")[0];
 
 checkboxlist.addEventListener("change", (event) => {
@@ -44,38 +35,96 @@ checkboxlist.addEventListener("change", (event) => {
     } else {
         strengthLevel.textContent = ""
     }
-
-
-    console.log(event.target.id);
-    console.log(event.target.checked);
-let checked = event.target.id;
-console.log("checked:",checked);
 })
 
-
+let generatedPassword = "";
+let generateFrom = "";
 
 function generatePassword (length) {
-    let password = [];
-    let uppercase = document.getElementById("uppercase").checked
-    let lovercase = document.getElementById("lowercase").checked
-    let number = document.getElementById("numbers").checked
-    let symbols = document.getElementById("symbols").checked
+    
+    
+//    if (generatedPassword) {
+//        let generatedPassword = "";
+//       return generatedPassword;
+//    }
+
+    let generatedPassword = "";
+    let generateFrom = "";
+
+    console.log("type of generatedPassword:", typeof generatedPassword)
 
 
-    console.log(uppercase)
-    if (uppercase) {
-        console.log("uppercase TRUE")
+//
+//    let generateFrom = "";
+
+
+    let uppercaseCheck = document.getElementById("uppercase").checked
+    let lowercaseCheck = document.getElementById("lowercase").checked
+    let numberCheck = document.getElementById("numbers").checked
+    let symbolsCheck = document.getElementById("symbols").checked
+
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowercase = "abcdefghijklmnopqrstuvwxy";
+    var numbers = "0123456789";
+    var symbols = "!@#$%^&*()";
+  
+    if (uppercaseCheck) {
+        generateFrom += uppercase;
+    }
+    if (lowercaseCheck) {
+        generateFrom += lowercase;
+    }
+    if (numberCheck) {
+        generateFrom += numbers;
+    }
+    if (symbolsCheck) {
+        generateFrom += symbols;
     }
 
- //   var lowercase = "abcdefghijklmnopqrstuvwxy";
- //   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
- //   var numbers = "0123456789";
- //   var symbols = "!@#$%^&*()";
-    
+// add error when nothing is checked or 0 length
 
+
+for ( let i = 0; i < length; i++ ) {
+    generatedPassword += generateFrom.charAt(Math.floor(Math.random() * generateFrom.length));
+ }
+
+let display = document.getElementsByClassName("generated-password")[0];
+display.textContent = "";
+
+console.log (generatedPassword);
+
+//if (generatedPassword) {
+//    display.innerHTML = generatedPassword;
+//}
+display.innerHTML = generatedPassword;
+
+//if (display.className != "color-white") {
+    display.className = "color-white";
+//}
+
+generateFrom = "";
 }
 
 const generateButton = document.getElementsByTagName("button")[0];
 generateButton.addEventListener("click", () => {
     generatePassword(characterLength.textContent);
 })
+
+
+
+
+
+
+
+  //not working
+  document.getElementsByClassName("copied")[0].addEventListener("click", () => {
+    document.getElementsByClassName("copy-icon")[0].className = "copied copied-visible";
+  });
+
+
+
+// range bar
+  document.getElementById("password-length-range").oninput = function() {
+    var value = (this.value-this.min)/(this.max-this.min)*100
+    this.style.background = 'linear-gradient(to right, #A4FFAF 0% ' + value + '%,  ' + value + '%, #18171F 100%)'
+  };
