5685ed8468a0e52c0d90527469648bccc2ed5ad1
Marek Vcelak <vcelak.marek@gmail.com>
2022-11-03 19:35:21 +0100

    Update password_generate.js
    add checkboxCheck object instead of variables

--- scripts/password_generate.js
+++ scripts/password_generate.js
@@ -48,23 +48,29 @@ function generatePassword (length) {
     let generatedPassword = "";
     let generateFrom = "";
 
-    const uppercaseCheck = document.getElementById("uppercase").checked
-    const lowercaseCheck = document.getElementById("lowercase").checked
-    const numberCheck = document.getElementById("numbers").checked
-    const symbolsCheck = document.getElementById("symbols").checked
+
+   // const uppercaseCheck = document.getElementById("uppercase").checked
+   // const lowercaseCheck = document.getElementById("lowercase").checked
+   // const numberCheck = document.getElementById("numbers").checked
+   // const symbolsCheck = document.getElementById("symbols").checked
     
+    const checkboxCheck = {uppercaseCheck: document.getElementById("uppercase").checked,
+        lowercaseCheck:document.getElementById("lowercase").checked,
+        numberCheck: document.getElementById("numbers").checked,
+        symbolsCheck: document.getElementById("symbols").checked}
+
     const characters = {uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ", lowercase: "abcdefghijklmnopqrstuvwxy", numbers: "0123456789", symbols: "!@#$%^&*()"};
 
-    if (uppercaseCheck) {
+    if (checkboxCheck.uppercaseCheck) {
         generateFrom += characters.uppercase;
     }
-    if (lowercaseCheck) {
+    if (checkboxCheck.lowercaseCheck) {
         generateFrom += characters.lowercase;
     }
-    if (numberCheck) {
+    if (checkboxCheck.numberCheck) {
         generateFrom += characters.numbers;
     }
-    if (symbolsCheck) {
+    if (checkboxCheck.symbolsCheck) {
         generateFrom += characters.symbols;
     }
 
